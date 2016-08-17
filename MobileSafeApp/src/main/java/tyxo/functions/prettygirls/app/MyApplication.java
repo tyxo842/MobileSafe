20690b64f429cc0341850927e7fb5a3f91f8f26a
tyxo842 <tyxo842@163.com>
2016-08-17 17:24:28 +0800

    添加美图加载(Rxjava+Retrofit) , 图片来自 干货集中营 , 修饰...调整 aboutActivity+链接时间6s

--- MobileSafeApp/src/main/java/tyxo/functions/prettygirls/app/MyApplication.java
+++ MobileSafeApp/src/main/java/tyxo/functions/prettygirls/app/MyApplication.java
@@ -1,18 +1,19 @@
 package tyxo.functions.prettygirls.app;
 
+import android.app.Application;
+
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
 import tyxo.functions.prettygirls.app.exception.LocalFileHandler;
 import tyxo.functions.prettygirls.util.LogUtil;
 import tyxo.functions.prettygirls.util.ToastUtil;
-import tyxo.mobilesafe.base.BaseApplication;
 
 
 /**
  * Created by oracleen on 2016/6/28.
  */
-public class MyApplication extends BaseApplication {
+public class MyApplication extends Application {
 
     private static MyApplication mApplication;
     public static String currentGirl = "http://ww2.sinaimg.cn/large/610dc034jw1f5k1k4azguj20u00u0421.jpg";
@@ -35,7 +36,7 @@ public class MyApplication extends BaseApplication {
 
     public static OkHttpClient defaultOkHttpClient() {
         OkHttpClient client = new OkHttpClient.Builder()
-                .connectTimeout(3, TimeUnit.SECONDS)
+                .connectTimeout(6, TimeUnit.SECONDS)
                 .writeTimeout(3, TimeUnit.SECONDS)
                 .readTimeout(3, TimeUnit.SECONDS)
                 .build();
